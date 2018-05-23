e5bdf08e59d09165bc2b848e6890d6f02bdd76b7
Kevin Yang <kevin7616@gmail.com>
2018-05-23 21:18:00 +1200

    Updated balance in speed of drop

--- core/src/com/kevin/game/FlappyBird.java
+++ core/src/com/kevin/game/FlappyBird.java
@@ -58,7 +58,7 @@ public class FlappyBird extends ApplicationAdapter {
 
 
 			if (Gdx.input.justTouched()) {
-				velocity = -32;
+				velocity = -30;
 
 				tubeOffSet = (randomGenerator.nextFloat() - 0.5f) * (Gdx.graphics.getHeight() - gap - 100);
 			}
